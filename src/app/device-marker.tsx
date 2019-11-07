859025b8ec02bae89f405242b4d815ad71b2f308
Dennis Cual <dennis.cual@ombori.com>
2019-11-07 17:13:43 +0100

    feat(interactive-maps): add pulse animatinn to device marker

--- src/app/device-marker.tsx
+++ src/app/device-marker.tsx
@@ -57,9 +57,55 @@ const DeviceMarker: React.FC<{ x: number; y: number; angle: number }> = ({
         </filter>
       </defs>
       <g filter="url(#filter0_d)">
-        <circle cx={x} cy={y} r="86" fill="white" fillOpacity="0.85" />
+        <circle cx={x} cy={y} r="80" fill="white" fillOpacity="0.85" />
       </g>
-      <circle cx={x} cy={y} r="44.5" fill="#3555FF" />
+      <circle cx={x} cy={y} r="45" fill="#3555FF" />
+      <g
+        fill="none"
+        fillRule="evenodd"
+        strokeWidth="2"
+        stroke="#0828d3"
+        strokeOpacity="0.7"
+      >
+        <circle cx={x} cy={y} r="150">
+          <animate
+            attributeName="r"
+            begin="0s"
+            dur="3s"
+            values="0;150"
+            keyTimes="0;1"
+            keySplines="0.1,0.2,0.3,1"
+            calcMode="spline"
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="stroke-opacity"
+            begin="0s"
+            dur="3s"
+            values="0;.3;.3;0"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx={x} cy={y} r="45">
+          <animate
+            attributeName="r"
+            begin="-1s"
+            dur="3s"
+            values="0;150"
+            keyTimes="0;1"
+            keySplines="0.1,0.2,0.3,1"
+            calcMode="spline"
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="stroke-opacity"
+            begin="-1s"
+            dur="3s"
+            values="0;.3;.3;0"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </g>
     </g>
   )
 }
